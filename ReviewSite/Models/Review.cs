3ade60eb3b45c7e5b3d5ce8d02fd4952cf32bc9f
Sam Jacobson <samuel.c.jacobson@gmail.com>
2017-07-28 11:05:13 -0400

    created models and controllers

---
+++ ReviewSite/Models/Review.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ReviewSite.Models
+{
+    public class Review
+    {
+        [Key]
+        public int ReviewID { get; set; }
+        public string ReviewTitle { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime ReviewDate { get; set; }
+        public string ReviewContent { get; set; }
+        public string ReviewerName { get; set; }
+
+        [ForeignKey("Category")]
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
